9e4dd33c935695b7569164b1e6e2d206656e1d4c
jangle <shubhamjangale0000@gmail.com>
2020-10-01 07:56:51 +0530

    Refactor : Added functionality to validate Password exactly 1 special character

--- UserRegistration.java
+++ UserRegistration.java
@@ -55,7 +55,7 @@ public class UserRegistration {
 		
 	}
 	private String validatePassword(String pass) {
-		String expression = "(?=.*[A-Z])(?=.*[0-9])";	//Pattern for Password
+		String expression = "(?=.*[A-Z])(?=.*[0-9])(?=.[@#$%^&*])[A-Z-a-z0-9]{8,}$";	//Pattern for Password
 		Pattern pattern = Pattern.compile(expression);
 		Matcher match = pattern.matcher(pass);
 		if(match.find()) {
